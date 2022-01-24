3811ac8a5c7dc6823d6ec3d2327a02aafa23ae35
hatdoggy <ondaprey@gmail.com>
2022-01-24 13:41:52 +0800

    australian branch

--- src/components/comments.js
+++ src/components/comments.js
@@ -16,18 +16,18 @@ const Comments = (props)=>{
     }
 
     return <div className="comments flx flx-jc-ce flx-ai-ce flx-col bg-pop fade pos-abs z-top h-100">
-        <div className="p-20 bg-red flx flx-jc-ce flx-ai-ce fade-t brd w-50 headCom pos-rel">
+        <div className="p-20 bg-blu flx flx-jc-ce flx-ai-ce fade-t brd w-50 headCom pos-rel">
             <h4 className="txt-wht txt-al-ce mont">{head}</h4>
             <Exit click={click}/>
         </div>
         <div className="w-80 flx flx-jc-sa flx-ai-ce flx-wrp fade-t comCont">
             {comments.map((elem,ndx)=>(
-                <div key={ndx} className={`comment w-30 h-50 m-t-2 m-b-2 flx flx-jc-ce flx-ai-ce brd ${ndx%2 === 0? "m-r-2 m-l-2 bg-red":" bg-grn"}`}>
+                <div key={ndx} className={`comment w-30 h-50 m-t-2 m-b-2 flx flx-jc-ce flx-ai-ce p-20 brd ${ndx%2 === 0? "m-r-2 m-l-2 bg-blu":" bg-grn"}`}>
                     <img src={elem.src} alt={elem.alt} className="w-30"/>
                     <div className="flx flx-col flx-jc-sa w-70 m-l-5">
-                        <p className={`mont txt-al-l ${ndx%2 === 0 ?"txt-wht":"txt-red"}`}>{elem.name}</p>
-                        <small className={`lato txt-al-l ${ndx%2 === 0 ?"txt-wht":"txt-red"}`}>{elem.time}</small>
-                        <p className={`lato txt-al-l ${ndx%2 === 0 ?"txt-wht":"txt-red"}`}>{elem.mes}</p>
+                        <p className={`mont txt-al-l ${ndx%2 === 0 ?"txt-wht":"txt-blu"}`}>{elem.name}</p>
+                        <small className={`lato txt-al-l ${ndx%2 === 0 ?"txt-wht":"txt-blu"}`}>{elem.time}</small>
+                        <p className={`lato txt-al-l ${ndx%2 === 0 ?"txt-wht":"txt-blu"}`}>{elem.mes}</p>
                     </div>
                 </div>
             ))}
